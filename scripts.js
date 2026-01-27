758cd1f7bbd5c8d4cfbddb9a1c39718672b03686
LuanaChavesFreire <luanachavesfreire2006@gmail.com>
2026-01-27 19:32:34 -0300

    validação de tags

--- scripts.js
+++ scripts.js
@@ -6,6 +6,17 @@ const inputTag = document.getElementById("categoria");
 const listaTags = document.getElementById("listaTags");
 const mensagemTagVazia = document.getElementById("mensagemTagVazia");
 
+const tagsDisponiveis = [
+  "Front-end",
+  "Programação",
+  "Data-science",
+  "Full-stack",
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+];
+
 uploadBtn.addEventListener("click", () => {
   imageUpload.click();
 });
@@ -36,21 +47,6 @@ imageUpload.addEventListener("change", async (evento) => {
   }
 });
 
-inputTag.addEventListener("keypress", (evento) => {
-  if (evento.key === "Enter") {
-    evento.preventDefault();
-    const tagTexto = inputTag.value.trim();
-    if (inputTag !== "") {
-      const novaTag = document.createElement("li");
-      novaTag.innerHTML = `<p>${tagTexto}</p> <img src=${"img/close-black.svg"} class="removeTag">`;
-      listaTags.appendChild(novaTag);
-      inputTag.value = "";
-    }
-  }
-
-  verificaListaTag();
-});
-
 listaTags.addEventListener("click", (evento) => {
   if (evento.target.classList.contains("removeTag")) {
     const tagRemovida = evento.target.parentElement;
@@ -70,3 +66,37 @@ function verificaListaTag() {
 }
 
 verificaListaTag();
+
+async function verificaTags(tagTexto) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(tagsDisponiveis.includes(tagTexto));
+    }, 1000);
+  });
+}
+
+inputTag.addEventListener("keypress", async (evento) => {
+  if (evento.key === "Enter") {
+    evento.preventDefault();
+    const tagTexto = inputTag.value.trim();
+    if (inputTag !== "") {
+      try {
+        const tagExiste = await verificaTags(tagTexto);
+        if (tagExiste) {
+          const novaTag = document.createElement("li");
+          novaTag.innerHTML = `<p>${tagTexto}</p> <img src=${"img/close-black.svg"} class="removeTag">`;
+          listaTags.appendChild(novaTag);
+          inputTag.value = "";
+        } else {
+          alert("Tag não foi encontrada");
+        }
+      } catch (error) {
+        console.error("Erro ao verificara existenci da tag.");
+        alert("Erro ao verificar a existencia da tag, verifique o console.");
+      }
+    }
+  }
+
+  verificaListaTag();
+});
+
